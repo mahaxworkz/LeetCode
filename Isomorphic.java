c8f487626cb7a2107c0b81860e90320ce1d787af
unknown <mahalakshmi.xworkz@gmail.com>
2023-10-02 21:56:27 +0100

    isomorpic

---
+++ Isomorphic.java
@@ -0,0 +1,28 @@
+class  Isomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        if(s==null || t==null || s.length()!=t.length()){
+            return false;
+        }
+
+        Map<Character,Character> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+          char a=s.charAt(i);
+          char b=t.charAt(i);
+          if(map.containsKey(a)){
+              if(map.get(a)!=b){
+                  return false;
+              }
+
+          }
+          else{
+              if(map.containsValue(b))
+              return false;
+              map.put(a,b);
+          }
+          
+          
+        }
+        
+        return true;
+    }
+}
