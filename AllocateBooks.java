43d581599e7310979737383f34546b1b2ad30022
unknown <mahalakshmi.xworkz@gmail.com>
2024-01-29 21:01:10 +0530

    LeetCode

---
+++ AllocateBooks.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.*; 
+public class  AllocateBooks{
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+       if (m > n)
+            return -1;
+
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int students = countStudents(arr, mid);
+            if (students > m) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    public static int countStudents(ArrayList<Integer> arr, int pages) {
+        int n = arr.size(); // size of array
+        int students = 1;
+        long pagesStudent = 0;
+        for (int i = 0; i < n; i++) {
+            if (pagesStudent + arr.get(i) <= pages) {
+                // add pages to current student
+                pagesStudent += arr.get(i);
+            } else {
+                // add pages to next student
+                students++;
+                pagesStudent = arr.get(i);
+            }
+        }
+        return students;
+    }
+}
